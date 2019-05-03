29d8c44b0df2355328987fd59270df8a9149ddfe
Ryan Gross <ryangross123@icloud.com>
2019-05-03 11:51:04 -0500

    finished again

--- Components/Speakers.js
+++ Components/Speakers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Alert, FlatList, StyleSheet } from 'react-native';
-import { ListItem, withTheme } from 'react-native-elements';
+import { ListItem } from 'react-native-elements';
 import firebase, { db } from '../Services/Firebase';
 import APIkeys from '../Constants/APIkeys';
 
